edc4b8bc38a70046b7376169c36523add4cabe88
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-01-03 13:06:36 -0800

    Bug 1419116 - Reassemble records into a single submission structure
    This adds `RichMessage.toJson`. Fields are often extracted using a heka
    filter as a way of flattening the payload. This test is the expected
    behavior based on `python_moztelemetry`.

--- src/test/scala/com/mozilla/telemetry/heka/MessageTest.scala
+++ src/test/scala/com/mozilla/telemetry/heka/MessageTest.scala
@@ -7,6 +7,8 @@
 package com.mozilla.telemetry.heka
 
 import org.scalatest.{FlatSpec, Matchers}
+import org.json4s._
+import org.json4s.jackson.JsonMethods.parse
 
 class MessageTest extends FlatSpec with Matchers {
   "Fields" can "be parsed" in {
@@ -16,5 +18,32 @@ class MessageTest extends FlatSpec with Matchers {
     fields("bool").asInstanceOf[Boolean] should be (true)
     fields("double").asInstanceOf[Double] should be (4.2)
     fields("integer").asInstanceOf[Long] should be (42)
+  }
+
+  "Message" can "be represented as valid JSON" in {
+    val json = Resources.message.asJson
+    val doc = parse(json)
+
+    // check that the field has been casted correctly
+    doc \\ "meta" \\ "integer" should be (JInt(42))
+
+    doc \\ "gamma" should be (JString("3"))
+
+    // single level of unnesting
+    doc \\ "subfield" \\ "delta" should be (JString("4"))
+
+    // multiple level of unnnesting
+    doc \\ "nested" \\ "subfield" \\ "epsilon" should be (JString("5"))
+
+    // count the number of keys in partiallyExtracted
+    (doc \\ "partiallyExtracted").values.asInstanceOf[Map[String, Any]].keys.size should be (3)
+
+    // partial extraction
+    doc \\ "partiallyExtracted" \\ "nested" \\ "zeta" should be (JString("6"))
+  }
+
+  it can "handle documents without extracted fields" in {
+    val message = RichMessage("uuid", Resources.message.fieldsAsMap.filterKeys(k => !k.contains(".")), None)
+    parse(message.asJson)
   }
 }
